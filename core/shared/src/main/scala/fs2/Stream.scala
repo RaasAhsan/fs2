ae8397454bd8702d6180ec914e0f332b239d7bac
Raas Ahsan <raas.ahsan@gmail.com>
2020-08-22 23:08:29 -0500

    else

--- core/shared/src/main/scala/fs2/Stream.scala
+++ core/shared/src/main/scala/fs2/Stream.scala
@@ -564,6 +564,7 @@ final class Stream[+F[_], +O] private[fs2] (private val underlying: Pull[F, O, U
     } yield {
       def runR: F2[Unit] =
         that.interruptWhen(interrupt.get.attempt).compile.drain.attempt.flatMap { r =>
+          println("Reached the end!")
           doneR.complete(r) >> {
             if (r.isLeft)
               interrupt
