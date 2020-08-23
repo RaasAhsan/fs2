90d2c21b6d58d098314ae1311ed30a66a9c86469
Raas Ahsan <raas.ahsan@gmail.com>
2020-08-23 00:01:29 -0500

    Asdf

--- core/shared/src/test/scala/fs2/StreamSuite.scala
+++ core/shared/src/test/scala/fs2/StreamSuite.scala
@@ -374,7 +374,7 @@ class StreamSuite extends Fs2Suite {
 
     test("concurrently") {
       testCancelation {
-        Stream.eval[IO, Unit](IO(())).repeat.concurrently(Stream.constant[IO, Int](1))
+        Stream.constant[IO, Int](1).concurrently(Stream.constant[IO, Int](1))
       }
     }
 
