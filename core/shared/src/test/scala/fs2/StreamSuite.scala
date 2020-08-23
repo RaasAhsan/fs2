170017c3217ba54d679a54709fb44f3d405bfd15
Raas Ahsan <raas.ahsan@gmail.com>
2020-08-22 21:16:01 -0500

    change constant stream

--- core/shared/src/test/scala/fs2/StreamSuite.scala
+++ core/shared/src/test/scala/fs2/StreamSuite.scala
@@ -374,7 +374,7 @@ class StreamSuite extends Fs2Suite {
 
     test("concurrently") {
       testCancelation {
-        constantStream.concurrently(constantStream)
+        constantStream.concurrently(Stream.constant(1).evalTap(_ => IO.sleep(1.milli)))
       }
     }
 
